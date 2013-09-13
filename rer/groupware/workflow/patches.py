2831aed784faa3eb5e5227cf631260da3103cf97
cekk <andrea.cecchi85@gmail.com>
2013-09-13 08:40:58 +0000

    fixed forum wf

--- rer/groupware/workflow/patches.py
+++ rer/groupware/workflow/patches.py
@@ -10,9 +10,9 @@ See https://github.com/collective/Products.Ploneboard/issues/22
 try:
     from Products.Ploneboard.content import PloneboardForum
     from Products.Ploneboard.content import PloneboardComment
-    
+
     from rer.groupware.workflow import logger
-    
+
     try:
         PloneboardForum.schema['allowEditComment'].widget.visible = {'edit': 'invisible',
                                                                      'view': 'invisible'}
@@ -20,5 +20,6 @@ try:
     except KeyError:
         pass
     PloneboardComment.PloneboardComment.__ac_local_roles_block__ = False
+    logger.warning('Patching Ploneboard: disabling __ac_local_roles_block__ for comments')
 except ImportError:
     pass
