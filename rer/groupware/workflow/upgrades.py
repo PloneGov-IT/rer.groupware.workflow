72ec816a763df1a154f32b437725bd7e18ff3fc3
cekk <andrea.cecchi85@gmail.com>
2014-04-18 08:53:14 +0000

    EditorAdv can now delete Forum comments

---
+++ rer/groupware/workflow/upgrades.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+from Products.CMFCore.utils import getToolByName
+from rer.groupware.workflow import logger
+
+default_profile = 'profile-rer.groupware.workflow:default'
+
+
+def to_1100(context):
+    """
+    """
+    logger.info('Upgrading rer.groupware.workflow to version 2.1.1')
+    context.runImportStepFromProfile(default_profile, 'workflow')
+    logger.info('Updated workflows')
+    wf_tool = getToolByName(context, 'portal_workflow')
+    logger.info('Starting update security settings')
+    wf_tool.updateRoleMappings()
+    logger.info('Update security settings finish.')
